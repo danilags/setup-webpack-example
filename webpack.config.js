9904fb9b3fb0e6c9b0c869e98553f453ca9a18ff
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-02-26 11:40:00 +0700

    setup final config webpack to folder dist, still get error webpack-cli

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
-export default function() {
-    let el = document.createElement('h1');
-    el.innerHTML = 'Hello Webpack!!!';
-    return el; 
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry : ['./src/main.js','./src/sass/main.scss'],
+  output:{
+    path : path.resolve(__dirname,'./dist/'),
+    filename : 'js/app.bundle.js'
+  },
+  module : {
+    rules : [
+      {
+        test : /\.(sass|scss)$/,
+        loader : ExtractTextPlugin.extract(['css-loader','sass-loader'])
+      }
+    ]
+  },
+  plugins : [
+    new ExtractTextPlugin({
+      filename : 'css/app.bundle.css',
+      allChunks :true
+    }),
+    new HtmlWebpackPlugin({
+      title : 'Front End with Webpack'
+    })
+  ]
 }
-
